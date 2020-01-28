bd00b2047e92a6609181d26535588c5ff33dd031
陈百见 <602141901@qq.com>
2020-01-28 22:33:14 +0800

    读物

---
+++ chapter3/exercise3-8.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int ToolRandInt(int min, int max)
+{
+	srand((unsigned)time(NULL));
+	int r = rand() % (max - min + 1) + min;
+	return r;
+}
+class CardDeck
+{
+private:
+	int cards[52];
+	int currentCrd;
+public:
+	//构造函数
+	CardDeck(void);
+	//洗牌
+	void Shuffle(void);
+	//返回下一张牌
+	int GetCard(void);
+	//梅花在0-12， 方块在13-25，红心在26-38，黑桃在39-51。每个花色中
+	//第一张为A，最后三张为J、Q、K.，将牌C输出为“花色 牌值”
+	void PrintCard(int c);
+};
+CardDeck::CardDeck(void)
+{
+	int length = sizeof(cards) / sizeof(cards[0]);
+	cout << length << endl;;
+	for (int i = 0; i < length; i++)
+		cards[i] = i;
+	Shuffle();
+}
+void CardDeck::Shuffle(void)
+{
+	int length = sizeof(cards) / sizeof(cards[0]);
+	cout << length << endl;;
+	for (int j = 0; j < length; j++)
+	{
+		int randnumber = ToolRandInt(j, length - 1);
+		//交换两张牌
+		cards[j] = randnumber;
+		cards[randnumber] = j;
+	}
+}
+int CardDeck::GetCard(void)
+{
+
+}
+void CardDeck::PrintCard(int c)
+{
+	if (0 <= c & c < 13)
+		cout << "梅花 ";
+	else
+		if (13 <= c & c < 26)
+			cout << "方块 ";
+		else
+			if (26 <= c & c < 39)
+				cout << "红心 ";
+			else 
+				if (39 <= c & c < 52)
+					cout << "黑桃";
+				else
+					cout << "编号范围应该在0-51之间." << endl;
+					return;
+	switch (c%13)
+	{
+	case 0:
+		cout << "A";
+	case 1:
+		cout << "2";
+	case 2:
+		cout << "3";
+	case 3:
+		cout << "4";
+	case 4:
+		cout << "5";
+	case 5:
+		cout << "6";
+	case 6:
+		cout << "7";
+	case 7:
+		cout << "8";
+	case 8:
+		cout << "9";
+	case 9:
+		cout << "10";
+	case 10:
+		cout << "J";
+	case 11:
+		cout << "Q";
+	case 12:
+		cout << "K";
+	}
+	cout << endl;
+}
+int main()
+{
+	CardDeck cd;
+	//cout << cd.cards[0] << endl;
+	system("pause");
+}
