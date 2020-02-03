029df99f76c52787bc4c4e68634a2f62de0c9575
陈百见 <602141901@qq.com>
2020-02-03 22:47:26 +0800

    第五章代码

---
+++ chapter5/上机题/apqueue.h
@@ -0,0 +1,79 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+const int MaxPQSize = 50;
+
+class PQueue
+{
+private:
+	int count;
+	DataType pqlist[MaxPQSize];
+public:
+	PQueue(void);
+	void PQInsert(const DataType& item);
+	DataType PQDelete(void);
+	void ClearPQ(void);
+	int PQEmpty(void) const;
+	int PQLength(void) const;
+	int PQFull(void) const;
+};
+PQueue::PQueue(){}
+void PQueue::PQInsert(const DataType& item)
+{
+	if (count == MaxPQSize)
+	{
+		cerr << "Priority queue overflow!" << endl;
+		exit(1);
+	}
+	//并不是删除最前面一个，所以和 FIFO 队列不同
+	pqlist[count];
+	count++;
+}
+DataType PQueue::PQDelete(void)
+{
+	DataType min;
+	int i, minindex = 0;
+
+	if (count > 0)
+	{
+		min = pqlist[0];
+		for (i = 1; i < count; i++)
+		{
+			if (pqlist[i] < min) {
+				min = pqlist[i];
+				minindex = i;
+			}
+		}
+		pqlist[minindex] = pqlist[count - 1];
+		count--;
+	}
+	else {
+		cerr << "Deleting from empty pqueue!" << endl;
+		exit(1);
+	}
+	return min;
+}
+void PQueue::ClearPQ(void)
+{
+	count = 0;
+}
+int PQueue::PQEmpty(void) const
+{
+	if (count == 0)
+		return 1;
+	else
+		return 0;
+}
+int PQueue::PQLength(void) const
+{
+	return count;
+}
+int PQueue::PQFull(void) const
+{
+	if (count == MaxPQSize)
+		return 1;
+	else
+		return 0;
+}
